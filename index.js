4caa9b0779d1b7702af5083faf70646fd6c56c7f
borecjeborec1 <robingrundel@seznam.cz>
2022-02-18 12:53:20 +0100

    Update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { exec, execSync } = require('child_process');
 const { existsSync } = require('fs');
 
 exports.push = async function push(link, username) {
+  if (!link) return console.log('Please provide a link to push');
+  if (link == "-v") return console.log(require("./package.json").version);
   link = username ? `https://github.com/${username}/${link}.git` : link;
   if (existsSync(__dirname + "/.git")) {
     execSync('git add .');
@@ -9,7 +11,6 @@ exports.push = async function push(link, username) {
     execSync('git push');
     return
   }
-  if (!link) return console.log('Please provide a link to push');
   execSync('git init');
   execSync('git add .');
   execSync('git commit -m "Initial commit"');
